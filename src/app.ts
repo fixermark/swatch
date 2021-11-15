19a0c67aa5491e7ce19dd4fe0bf4f45cd82fa14d
Mark T. Tomczak <iam@fixermark.com>
2021-11-14 19:14:11 -0500

    Making our way towards working: server now serves public and build/, but build/client is set up for es6 modules and the browser cannot grok that. Time to bring in webpack...

--- src/app.ts
+++
@@ -1,7 +0,0 @@
-import * as http from "http";
-
-http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello new TypeScript server! Again!");
-    response.end();
-}).listen(8888);
---
+++ src/server/main.ts
@@ -0,0 +1,28 @@
+import { Server, Origins } from 'boardgame.io/server';
+import path from 'path';
+import serve from 'koa-static';
+import { TicTacToe } from '../game/Game';
+
+const server = Server({
+     games: [TicTacToe],
+     origins: [
+         'http://swatch-dev.fixermark.com',
+         'http://swatch.fixermark.com',
+         Origins.LOCALHOST_IN_DEVELOPMENT,
+         Origins.LOCALHOST,
+     ],
+});
+// Build path relative to the server.js file
+const frontendJsPath = path.resolve(__dirname, '..');
+const frontendPublicPath = path.resolve(__dirname, '../../public');
+server.app.use(async (ctx, next) => {
+    console.log(`requesting ${ctx.request.path}`);
+    await next();
+});
+server.app.use(serve((frontendJsPath)));
+server.app.use(serve((frontendPublicPath)));
+
+
+server.run(8000, () => {
+    console.log('server on');
+});
