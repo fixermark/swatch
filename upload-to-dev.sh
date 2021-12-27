c894708dac7e70265c1e07145d05a13d0ac007d0
Mark T. Tomczak <iam@fixermark.com>
2021-12-27 09:56:32 -0500

    Added licensing rule to package and copyright text to upload script

--- upload-to-dev.sh
+++ upload-to-dev.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Copyright 2021 Mark T. Tomczak
+# Licensed under the MIT License (https://opensource.org/licenses/MIT)
+#
 # Remember, this purposefully does not sync your node_modules because the cost is too high
 # We should probably either webpack the server or copy package.json over and run npm i server-side
 scp -r build/* fixermark.com:~/swatch-dev.fixermark.com
